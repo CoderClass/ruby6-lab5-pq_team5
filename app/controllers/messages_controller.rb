6217317f891a70411a75cb5c1a9d5ccedb37fdfc
luuphu25 <luuphu25@gmail.com>
2017-03-15 19:50:27 +0700

    Mile stone 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,20 @@ class MessagesController < ApplicationController
 	def index
 		@messages = Message.all.order(created_at: :desc)
 	end
+
+  def create
+    @message = Message.new message_params
+    if @message.save 
+      flash[:sucess] = "Create message"      
+    else
+      flash[:error] = "Error #{@message.errors.full_messages.to_sentence}"
+      
+    end
+    redirect_to messages_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
